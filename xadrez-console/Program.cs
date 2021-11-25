f216b9a70359c7e211716a4e641b5e4e8e8f8742
Lucas Vieira <lucas208@ufrn.edu.br>
2021-11-25 16:49:56 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -11,21 +11,23 @@ namespace xadrez_console
 
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 2));
+                    Console.WriteLine();
 
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 2));
-
-                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 4));
-
-                Tela.imprimirTabuleiro(tab);
-
-               
+                    partida.executaMovimento(origem, destino);
+                }
+                
             }
             catch(TabuleiroException e)
             {
